3a6264ee24448c751028ef1cb289d4dd686f5e8a
Aakashgawde <aakashgawde21@gmail.com>
2022-09-05 20:47:12 +0530

    commit

--- Random_Password/main.py
+++ Random_Password/main.py
@@ -7,12 +7,16 @@ from random import randint
 #     i = chr(random.randint(65,90))
 #     password = password + i
 # print(password)
+
 #------------------------------------------------------------------------------------------------------------------------
 # Upper and lowercase password
 password = ""
-for i in range(5):
+for i in range(8):
+    #* produces random uppercase letter
     i = chr(random.randint(65, 90))
+    #* produces random lowercase letter
     j = chr(random.randint(65, 90)).lower()
     password = str(password) + i + j
 print(password)
 
+#! randint()--> The randint() method returns an integer number selected element from the specified range.s
